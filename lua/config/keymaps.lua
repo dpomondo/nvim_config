3d70d14e34c5c9b75e91050883dc398661a0563a
dpomondo <dpomondo@gmail.com>
2024-06-24 10:26:27 -0600

    inital

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,46 @@
+--[[
+      ___           ___                       ___           ___     
+     /__/|         /  /\          ___        /__/\         /  /\    
+    |  |:|        /  /:/_        /__/|      |  |::\       /  /::\   
+    |  |:|       /  /:/ /\      |  |:|      |  |:|:\     /  /:/\:\  
+  __|  |:|      /  /:/ /:/_     |  |:|    __|__|:|\:\   /  /:/~/::\ 
+ /__/\_|:|____ /__/:/ /:/ /\  __|__|:|   /__/::::| \:\ /__/:/ /:/\:\
+ \  \:\/:::::/ \  \:\/:/ /:/ /__/::::\   \  \:\~~\__\/ \  \:\/:/__\/
+  \  \::/~~~~   \  \::/ /:/     ~\~~\:\   \  \:\        \  \::/     
+   \  \:\        \  \:\/:/        \  \:\   \  \:\        \  \:\     
+    \  \:\        \  \::/          \__\/    \  \:\        \  \:\    
+     \__\/         \__\/                     \__\/         \__\/    
+      ___         ___     
+     /  /\       /  /\    
+    /  /::\     /  /:/_   
+   /  /:/\:\   /  /:/ /\  
+  /  /:/~/:/  /  /:/ /::\ 
+ /__/:/ /:/  /__/:/ /:/\:\
+ \  \:\/:/   \  \:\/:/~/:/
+  \  \::/     \  \::/ /:/ 
+   \  \:\      \__\/ /:/  
+    \  \:\       /__/:/   
+     \__\/       \__\/    
+--]]
+--
+--[[
+--	make the key do the thing
+--	see `:help vim.keymap.set()`
+--]]
+--
+vim.opt.hlsearch = true			-- highlight matches as you type
+-- <ESC> in Normal mode will remove search highlighting
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+------------ better buffer navigation ----------------------
+vim.keymap.set('n', '<leader>n', '<cmd>bnext<CR>')
+vim.keymap.set('n', '<leader>p', '<cmd>bprev<CR>')
+
+------------ masochists don't use arrows! ------------------
+vim.keymap.set({'n', 'i'}, '<Up>', '<nop>')
+vim.keymap.set({'n', 'i'}, '<Down>', '<nop>')
+vim.keymap.set({'n', 'i'}, '<Left>', '<nop>')
+vim.keymap.set({'n', 'i'}, '<Right>', '<nop>')
+
+-- no hitting the HELP key accidentally
+vim.keymap.set({'n', 'i', 'v'}, '<F1>', '<ESC>')
---
+++ lua/config/options.lua
@@ -0,0 +1,47 @@
+--[[
+      ___           ___                                 ___           ___     
+     /  /\         /  /\        ___       ___          /  /\         /__/\    
+    /  /::\       /  /::\      /  /\     /  /\        /  /::\        \  \:\   
+   /  /:/\:\     /  /:/\:\    /  /:/    /  /:/       /  /:/\:\        \  \:\  
+  /  /:/  \:\   /  /:/~/:/   /  /:/    /__/::\      /  /:/  \:\   _____\__\:\ 
+ /__/:/ \__\:\ /__/:/ /:/   /  /::\    \__\/\:\__  /__/:/ \__\:\ /__/::::::::\
+ \  \:\ /  /:/ \  \:\/:/   /__/:/\:\      \  \:\/\ \  \:\ /  /:/ \  \:\~~\~~\/
+  \  \:\  /:/   \  \::/    \__\/  \:\      \__\::/  \  \:\  /:/   \  \:\  ~~~ 
+   \  \:\/:/     \  \:\         \  \:\     /__/:/    \  \:\/:/     \  \:\     
+    \  \::/       \  \:\         \__\/     \__\/      \  \::/       \  \:\    
+     \__\/         \__\/                               \__\/         \__\/    
+      ___     
+     /  /\    
+    /  /:/_   
+   /  /:/ /\  
+  /  /:/ /::\ 
+ /__/:/ /:/\:\
+ \  \:\/:/~/:/
+  \  \::/ /:/ 
+   \__\/ /:/  
+     /__/:/   
+     \__\/    
+--]]
+
+--[[--------------------------------------------------------	
+--	see 	`:help vim.opt.  
+--		`:h.opt.tions-list`
+--]]--------------------------------------------------------
+
+------------ looks & feels ---------------------------------
+vim.opt.number = true		-- line numbers in the gutter
+vim.opt.numberwidth = 5		-- keep consistent
+vim.opt.signcolumn = 'yes'	-- signs in the gutter
+vim.opt.cursorline = true		-- where am i?
+vim.opt.scrolloff = 5		-- keep some context when scrolling
+
+vim.opt.breakindent = true	-- line breaks maintain indentation levels
+
+------------ buffer behavior -------------------------------
+vim.opt.undofile = true		-- save undo history
+vim.opt.hidden = true		-- allow files to hide
+
+------------ searches & rescues ----------------------------
+vim.opt.hlsearch = true			-- highlight matches as you type
+vim.opt.ignorecase = true	-- searches ignore capiTALizatiOn
+vim.opt.smartcase = true
