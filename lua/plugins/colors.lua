72bb6bc29e1ef6da26a350de142754dbeef14eb5
Casey Opdahl <dpomondo@gmail.com>
2025-07-09 19:49:09 -0600

    added italics back to material colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,38 +1,21 @@
-color_o = {
-	{
-		'folke/tokyonight.nvim',
-		priority = 1000,
-		-- init = function()
-		-- 	vim.cmd.colorscheme 'tokyonight-storm'
-		-- end,
+return {
+    {
+        'folke/tokyonight.nvim',
+        priority = 1000,
     },
-    { 
-        "catppuccin/nvim", 
-        name = "catppuccin", 
-        priority = 1000 ,
-		-- init = function()
-		--           vim.cmd.colorscheme "catppuccin"
-		-- end,
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
     },
     {
         "marko-cerovac/material.nvim",
-		priority = 1000,
-		-- init = function()
-		-- 	-- vim.cmd.colorscheme 'material-oceanic'
-		--     local host = vim.fn.hostname()
-		--     -- vim.notify("local name is: " .. host)
-		--     local colors = ""
-		--     if host == "squirrel2" then
-		--       colors = "material-oceanic" 
-		--     else
-		--       colors = "tokyonight-storm"
-		--     end
-		--     local status_ok, _ = pcall(vim.cmd.colorscheme, colors)
-		--     if not status_ok then
-		--       vim.notify("error loading " .. colors .. " colorscheme")
-		--     end
-		--     vim.notify("local name is: " .. host .. " and colorschem is " .. colors)
-		-- end,
+        priority = 1000,
+        opts = {
+                styles = { -- Give comments style such as bold, italic, underline etc.
+                    comments = {  italic = true  },
+                }
+        },
     },
+
 }
-return color_o
