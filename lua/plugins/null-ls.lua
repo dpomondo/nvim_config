a6d6adc562884f234d05ec3d98df87b43c916c5e
Casey Opdahl <dpoyesac@gmail.com>
2026-07-30 10:45:30 -0600

    added additional sources to null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -19,10 +19,19 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+                -- lua
 				null_ls.builtins.formatting.stylua,
+                -- python
 				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.diagnostics.pylint,
+                -- c and cmake
+				null_ls.builtins.formatting.clang_format,
+                null_ls.builtins.diagnostics.cmake_lint,
+                null_ls.builtins.formatting.cmake_format,
+                -- am bad speller smtimes so mebbe this help?!?
+                null_ls.builtins.completion.spell,
+                -- lets see how this does?
+                null_ls.builtins.completion.luasnip,
 			}, -- end sources
 		})
 	end,
