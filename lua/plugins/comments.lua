21a00aee5b55b740b71fbeeb5d3c5f537c569232
Casey Opdahl <dpoyesac@gmail.com>
2026-07-30 10:18:18 -0600

    tried & rejected a commenting plugin

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,10 @@
+return {
+  -- we killed this one cos it made things worse: it added keybinds for 
+  -- multi-line comments but some filetypes (cmake and make specifically) 
+  -- no longer worked
+  -- 
+  -- 'numToStr/Comment.nvim',
+  -- opts = {
+  --     -- add any options here
+  -- }
+}
