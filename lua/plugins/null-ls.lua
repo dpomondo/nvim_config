c7377aff34585ba020c80304dbe12033e9ee3efd
Casey Opdahl <dpoyesac@gmail.com>
2026-08-06 14:34:36 -0600

    added .clang-format search to null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -14,25 +14,34 @@
 --]]
 
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-                -- lua
-				null_ls.builtins.formatting.stylua,
-                -- python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.pylint,
-                -- c and cmake
-				null_ls.builtins.formatting.clang_format,
-                null_ls.builtins.diagnostics.cmake_lint,
-                null_ls.builtins.formatting.cmake_format,
-                -- am bad speller smtimes so mebbe this help?!?
-                null_ls.builtins.completion.spell,
-                -- lets see how this does?
-                null_ls.builtins.completion.luasnip,
-			}, -- end sources
-		})
-	end,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls") -- none is null
+    null_ls.setup({
+      sources = {
+        -- lua
+        null_ls.builtins.formatting.stylua,
+        -- python
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.diagnostics.pylint,
+        -- c and cmake
+        null_ls.builtins.formatting.clang_format.with({
+          -- this fails for some strange reason, and even worse it fails INCONSISTENTLY
+          -- extra_args = {"--style=file:" .. "/home/dpoyesac/.config/_clang-format"},
+          -- extra_args = {"--style=file:" .. vim.fn.expand("~/.config/_clang-format")},
+
+          -- with this, a `.clang-format` or `_clang-format` needs to be in the project
+          -- root directory, or one the the directories above it
+          extra_args = { "--style=file" },
+        }),
+        null_ls.builtins.diagnostics.cmake_lint,
+        null_ls.builtins.formatting.cmake_format,
+        -- am bad speller smtimes so mebbe this help?!?
+        -- nevermind, give way too may suggestions
+        -- null_ls.builtins.completion.spell,
+        -- lets see how this does?
+        null_ls.builtins.completion.luasnip,
+      }, -- end sources
+    })
+  end,
 }
