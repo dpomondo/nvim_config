0dad7c13561e39e8a51de670e42c4c165d9045ab
Casey Opdahl <dpoyesac@gmail.com>
2026-08-27 09:22:31 -0600

    c lang tabs set to 4 for readability

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,17 +1,20 @@
 --[[
-      ___     
-     /  /\    
-    /  /:/    
-   /  /:/     
-  /  /:/  ___ 
+      ___
+     /  /\
+    /  /:/
+   /  /:/
+  /  /:/  ___
  /__/:/  /  /\
  \  \:\ /  /:/
-  \  \:\  /:/ 
-   \  \:\/:/  
-    \  \::/   
-     \__\/    
+  \  \:\  /:/
+   \  \:\/:/
+    \  \::/
+     \__\/
 --]]
-vim.notify("C language file, tabs set to 2")
+vim.notify("C language file")
+-- indent width of 2 makes scope hard to read, so we're breaking
+-- the OFFICIAL formatting rules.  Sorry C language style guyide writers!
+vim.notify("C language file")
 vim.opt.expandtab = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
